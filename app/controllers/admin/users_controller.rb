c23f8aec857ce627f00ee5ae35cc7b22a7c6c364
Meg Svetz <megsvetz@gmail.com>
2015-06-09 15:36:05 -0600

    added session stuff to app controller and helper

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,19 @@ class Admin::UsersController < ApplicationController
   end
 
   def create
+    @new_admin = Admin.new(new_admin_params)
+    if @new_admin.save
+      session[:admin_id] = @new_admin.id
+      redirect_to admin_books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def new_admin_params
+    params.require(:admin).permit(:username, :email, :password, :password_confirmation)
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_admin
+    @current_admin ||= Admin.where(id: session[:admin_id]).first
+  end
+  helper_method :current_admin
+
+  private
+
+  def authenticate_admin
+    unless session[:admin_id].present? && current_admin.present?
+      flash[:alert] = 'Sorry, you need to be logged in to do that.'
+      redirect_to login_path
+    end
+  end
+  
 end
